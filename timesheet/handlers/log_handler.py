8236f1cecd7ab6ca191aabe0a68534da9a83ac7d
jamesstidard <jamesstidard@gmail.com>
2016-02-28 16:31:14 +0000

    authentication

--- timesheet/handlers/log_handler.py
+++ timesheet/handlers/log_handler.py
@@ -6,6 +6,7 @@ from timesheet.utils.dot_dict import DotDict
 from tornado.httpclient import AsyncHTTPClient
 
 from timesheet.handlers.base_handler import BaseHandler
+from timesheet.utils.user_session import user_session
 
 __author__ = 'James Stidard'
 
@@ -17,84 +18,84 @@ class IncompleteLogException(ValueError):
 class LogHandler(BaseHandler):
 
     BASE_URL  = "https://projectsapi.zoho.com/restapi"
-    PORTAL_ID = 20557707
-    API_TOKEN = "c7a2105c8c9c8a23d27b0d839c6fbd76"
 
-    async def get(self):
-        with self.control.session as session:
-            logs = session.query(Log).all()
-            self.write([l.client_format for l in logs], format='json')
+    @user_session
+    async def get(self, session, user):
+        logs = session.query(Log).filter(Log.user_id == user.id).all()
+        self.write([l.client_format for l in logs], format='json')
 
-    async def post(self):
-        with self.control.session as session:
-            log = Log(
-                project_id=self.get_json_argument('project_id'),
-                task=self.get_json_argument('task'),
-                start=self.get_json_argument('start'),
-                end=self.get_json_argument('end', None),
-                billable=self.get_json_argument('billable', True),
-                notes=self.get_json_argument('notes', ''),
-                session=session,
-            )
-            session.commit()
+    @user_session
+    async def post(self, session, user):
+        log = Log(
+            project_id=self.get_json_argument('project_id'),
+            task=self.get_json_argument('task'),
+            start=self.get_json_argument('start'),
+            end=self.get_json_argument('end', None),
+            billable=self.get_json_argument('billable', True),
+            notes=self.get_json_argument('notes', ''),
+            user=user,
+            session=session,
+        )
+        session.commit()
 
-            try:
-                log.zoho_id = await self.insert_zoho_log(log)
-            except IncompleteLogException:
-                pass
-            else:
-                session.commit()
-                self.write(log.client_format, format='json')
-
-    async def put(self):
-        with self.control.session as session:
-            id  = self.get_json_argument('id')
-            log = session.query(Log).get(id)
-
-            for property_key in ['project_id', 'task', 'start', 'end', 'billable', 'notes']:
-                if property_key in self.json_arguments:
-                    value = self.get_json_argument(property_key)
-                    log.__setattr__(property_key, value)
-
-            if not log.zoho_id:
-                try:
-                    log.id = await self.insert_zoho_log(log)
-                except IncompleteLogException:
-                    pass
-            else:
-                try:
-                    log.id = await self.update_zoho_log(log)
-                except IncompleteLogException:
-                    self.delete_zoho_log(log)
-                    pass
-
+        try:
+            log.zoho_id = await LogHandler.insert_zoho_log(log)
+        except IncompleteLogException:
+            pass
+        else:
             session.commit()
             self.write(log.client_format, format='json')
 
-    async def delete(self):
-        with self.control.session as session:
-            id  = self.get_argument('id')
-            log = session.query(Log).get(id)
+    @user_session
+    async def put(self, session, user):
+        id  = self.get_json_argument('id')
+        log = session.query(Log).filter(Log.user_id == user.id).get(id)
 
-            if log.zoho_id:
-                self.delete_zoho_log(log.zoho_id)
+        for property_key in ['project_id', 'task', 'start', 'end', 'billable', 'notes']:
+            if property_key in self.json_arguments:
+                value = self.get_json_argument(property_key)
+                log.__setattr__(property_key, value)
 
-            session.delete(log)
-            session.commit()
+        if not log.zoho_id:
+            try:
+                log.id = await LogHandler.insert_zoho_log(log)
+            except IncompleteLogException:
+                pass
+        else:
+            try:
+                log.id = await LogHandler.update_zoho_log(log)
+            except IncompleteLogException:
+                LogHandler.delete_zoho_log(log)
+                pass
 
-            self.write("Success")
+        session.commit()
+        self.write(log.client_format, format='json')
 
-    async def insert_zoho_log(self, log):
+    @user_session
+    async def delete(self, session, user):
+        id  = self.get_argument('id')
+        log = session.query(Log).filter(Log.user_id == user.id).get(id)
+
+        if log.zoho_id:
+            LogHandler.delete_zoho_log(log.zoho_id)
+
+        session.delete(log)
+        session.commit()
+
+        self.write("Success")
+
+    @staticmethod
+    async def insert_zoho_log(log):
         if not log.completed:
             raise IncompleteLogException('Incomplete log entry. Cannot be submitted to Zoho.')
 
         client = AsyncHTTPClient()
         item   = urlencode(log.zoho_format)
         url    = '{base_url}/portal/{portal_id}/projects/{project_id}/logs/?authtoken={token}&{item}'.format(
-            base_url=self.BASE_URL,
-            portal_id=self.PORTAL_ID,
+            base_url=LogHandler.BASE_URL,
+            portal_id=log.user.portal_id,
             project_id=log.project_id,
-            token=self.API_TOKEN,
+            token=log.user.projects_token,
             item=item
         )
 
@@ -104,18 +105,19 @@ class LogHandler(BaseHandler):
         zoho_log = DotDict(body['timelogs']['generallogs'][0])
         return zoho_log.id
 
-    async def update_zoho_log(self, log):
+    @staticmethod
+    async def update_zoho_log(log):
         if not log.completed and log.zoho_id:
             raise IncompleteLogException('Incomplete log entry. Cannot be submitted to Zoho.')
 
         client = AsyncHTTPClient()
         item   = urlencode(log.zoho_format)
         url    = '{base_url}/portal/{portal_id}/projects/{project_id}/logs/{log_id}?authtoken={token}&{item}'.format(
-            base_url=self.BASE_URL,
-            portal_id=self.PORTAL_ID,
+            base_url=LogHandler.BASE_URL,
+            portal_id=log.user.portal_id,
             project_id=log.project_id,
             log_id=log.zoho_id,
-            token=self.API_TOKEN,
+            token=log.user.projects_token,
             item=item,
         )
 
@@ -125,14 +127,15 @@ class LogHandler(BaseHandler):
         zoho_log = DotDict(body['timelogs']['generallogs'][0])
         return zoho_log.id
 
-    async def delete_zoho_log(self, log):
+    @staticmethod
+    async def delete_zoho_log(log):
         client = AsyncHTTPClient()
         url    = '{base_url}/portal/{portal_id}/projects/{project_id}/logs/{log_id}?authtoken={token}&{item}'.format(
-            base_url=self.BASE_URL,
-            portal_id=self.PORTAL_ID,
+            base_url=LogHandler.BASE_URL,
+            portal_id=log.user.portal_id,
             project_id=log.project_id,
             log_id=log.zoho_id,
-            token=self.API_TOKEN,
+            token=log.user.projects_token,
         )
         result = await client.fetch(url, method='POST')
         assert result.code == 200
--- timesheet/server.py
+++ timesheet/server.py
@@ -7,10 +7,12 @@ from tornado.options import parse_command_line, define, options
 from tornado.web import Application
 
 from timesheet.control import Control
+from timesheet.handlers.login_handler import LoginHandler
 from timesheet.handlers.projects_handler import ProjectsHandler
 from timesheet.handlers.log_handler import LogHandler
 from timesheet.utils.orm_utils import heroku_db_url
 
+define("cookie_secret", 'timesheet-secret-please-dont-guess', str, help="key used to sign user cookies")
 define("debug", False, bool, help="run in debug mode")
 define("port", 8888, int, help="port to listen on")
 define("db_url", help="database url")
@@ -22,18 +24,20 @@ def main():
 
     parse_command_line()
 
+    secret = os.environ.get('COOKIE_SECRET', options.cookie_secret)
     debug  = os.environ.get("DEBUG", options.debug)
     port   = int(os.environ.get("PORT", options.port))
     db_url = os.environ.get("CLEARDB_DATABASE_URL", options.db_url)
     db_url = heroku_db_url(db_url)
 
     handlers = [
+        (r"/v1/rpc/login/?", LoginHandler),
         (r"/v1/resources/projects/?", ProjectsHandler),
         (r"/v1/resources/logs/?", LogHandler),
     ]
     settings = {
               "control": Control(db_url, pool_recycle=60),
-        "cookie_secret": 'timesheet-secret-please-dont-guess',
+        "cookie_secret": secret,
           "cookie_name": 'timesheet',
                 "debug": debug
     }
