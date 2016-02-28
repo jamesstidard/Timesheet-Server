cb3196b1c4b03b33a001eb1bda1581c530897962
jamesstidard <jamesstidard@gmail.com>
2016-02-28 15:16:33 +0000

    base handler

--- timesheet/handlers/log_handler.py
+++ timesheet/handlers/log_handler.py
@@ -4,7 +4,8 @@ from urllib.parse import urlencode
 from timesheet.model.model import Log
 from timesheet.utils.dot_dict import DotDict
 from tornado.httpclient import AsyncHTTPClient
-from tornado.web import RequestHandler, MissingArgumentError
+
+from timesheet.handlers.base_handler import BaseHandler
 
 __author__ = 'James Stidard'
 
@@ -13,30 +14,16 @@ class IncompleteLogException(ValueError):
     pass
 
 
-class LogHandler(RequestHandler):
+class LogHandler(BaseHandler):
 
     BASE_URL  = "https://projectsapi.zoho.com/restapi"
     PORTAL_ID = 20557707
     API_TOKEN = "c7a2105c8c9c8a23d27b0d839c6fbd76"
 
-    @property
-    def control(self):
-        return self.application.settings.get('control')
-
-    def prepare(self):
-        if self.request.headers.get( "Content-Type" ) == "application/json":
-            self.json_arguments = json.loads(self.request.body.decode('utf-8'))
-
-    _ARG_DEFAULT = []
-
-    def get_json_argument(self, name: str, default=_ARG_DEFAULT):
-        try:
-            return self.json_arguments[name]
-        except KeyError:
-            if default is self._ARG_DEFAULT:
-                raise MissingArgumentError(name)
-            else:
-                return default
+    async def get(self):
+        with self.control.session as session:
+            logs = session.query(Log).all()
+            self.write([l.client_format for l in logs], format='json')
 
     async def post(self):
         with self.control.session as session:
@@ -57,7 +44,45 @@ class LogHandler(RequestHandler):
                 pass
             else:
                 session.commit()
-                self.write(log.json)
+                self.write(log.client_format, format='json')
+
+    async def put(self):
+        with self.control.session as session:
+            id  = self.get_json_argument('id')
+            log = session.query(Log).get(id)
+
+            for property_key in ['project_id', 'task', 'start', 'end', 'billable', 'notes']:
+                if property_key in self.json_arguments:
+                    value = self.get_json_argument(property_key)
+                    log.__setattr__(property_key, value)
+
+            if not log.zoho_id:
+                try:
+                    log.id = await self.insert_zoho_log(log)
+                except IncompleteLogException:
+                    pass
+            else:
+                try:
+                    log.id = await self.update_zoho_log(log)
+                except IncompleteLogException:
+                    self.delete_zoho_log(log)
+                    pass
+
+            session.commit()
+            self.write(log.client_format, format='json')
+
+    async def delete(self):
+        with self.control.session as session:
+            id  = self.get_argument('id')
+            log = session.query(Log).get(id)
+
+            if log.zoho_id:
+                self.delete_zoho_log(log.zoho_id)
+
+            session.delete(log)
+            session.commit()
+
+            self.write("Success")
 
     async def insert_zoho_log(self, log):
         if not log.completed:
@@ -73,7 +98,7 @@ class LogHandler(RequestHandler):
             item=item
         )
 
-        result = await client.fetch( url, method='POST', allow_nonstandard_methods=True )
+        result = await client.fetch(url, method='POST', allow_nonstandard_methods=True)
         body = json.loads(result.body.decode('utf-8'))
 
         zoho_log = DotDict(body['timelogs']['generallogs'][0])
@@ -95,7 +120,7 @@ class LogHandler(RequestHandler):
         )
 
         result = await client.fetch(url, method='POST', allow_nonstandard_methods=True)
-        body   = json.loads(result.body.decode('utf-8'))
+        body = json.loads(result.body.decode('utf-8'))
 
         zoho_log = DotDict(body['timelogs']['generallogs'][0])
         return zoho_log.id
@@ -111,41 +136,3 @@ class LogHandler(RequestHandler):
         )
         result = await client.fetch(url, method='POST', allow_nonstandard_methods=True)
         assert result.code == 200
-
-    async def put(self):
-        with self.control.session as session:
-            id  = self.get_json_argument('id')
-            log = session.query(Log).get(id)
-
-            for property_key in ['project_id', 'task', 'start', 'end', 'billable', 'notes']:
-                if property_key in self.json_arguments:
-                    value = self.get_json_argument(property_key)
-                    log.__setattr__(property_key, value)
-
-            if log.zoho_id:
-                try:
-                    log.id = await self.update_zoho_log(log)
-                except IncompleteLogException:
-                    self.delete_zoho_log(log)
-                    pass
-            else:
-                try:
-                    log.id = await self.insert_zoho_log(log)
-                except IncompleteLogException:
-                    pass
-
-            session.commit()
-            self.write(log.json)
-
-    async def delete(self):
-        with self.control.session as session:
-            id  = self.get_json_argument('id')
-            log = session.query(Log).get(id)
-
-            if log.zoho_id:
-                self.delete_zoho_log(log.zoho_id)
-
-            session.delete(log)
-            session.commit()
-
-            self.write("Success")
