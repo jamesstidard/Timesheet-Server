afae2eeb9f2db0353c1a31979ecf753ff2af775d
jamesstidard <jamesstidard@gmail.com>
2016-03-12 23:39:24 +0000

    fix: packages name

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from distutils.core import setup
 setup(
     name='Timesheet',
     version='0.1.dev0',
-    packages=['Timesheet'],
+    packages=['timesheet'],
     long_description=open('README.md').read(),
 )
