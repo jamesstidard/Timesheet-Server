38ba149c1f11f55b7e7952f3165d2742af70b26e
jamesstidard <jamesstidard@gmail.com>
2016-03-13 17:55:41 +0000

    more default headers

--- timesheet/handlers/base_handler.py
+++ timesheet/handlers/base_handler.py
@@ -39,8 +39,11 @@ class BaseHandler(RequestHandler):
 
     def set_default_headers(self):
         origin = self.request.headers.get('Origin')
+
         self.set_header('Access-Control-Allow-Origin', origin)
-        self.set_header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+        self.set_header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS')
+        self.set_header('Access-Control-Allow-Headers', 'Content-Type, Depth, User-Agent, X-File-Size, X-Requested-With, X-Requested-By, If-Modified-Since, X-File-Name, Cache-Control')
+        self.set_header("Access-Control-Allow-Credentials", "true")
 
     def prepare(self):
         content_type = self.request.headers.get('Content-Type')
